9b8c4aab11db1a22277084d8aaf70662909eca60
rumeysakocaker <rumeysakocaker9@gmail.com>
2023-12-11 21:48:42 +0300

    Hastane_otomasyonu
    ana sayfa tasarımı yapıldı. Syntax hataları giderildi.

---
+++ anasayfa.php
@@ -0,0 +1,23 @@
+<?php
+include "header.php";
+?>
+
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href=style1.css>
+    <title>Hastane Otomasyonu</title>
+</head>
+<body>
+    <div class="adsoyad">
+        <h4> Sn. Ad Soyad</h4>
+    </div>
+<div class= "ortadiv" id="randevu_div">
+
+</div>
+    <div class="ortadiv" id="ailehekimi_div">
+    </div>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href=style1.css>
+    <title>Hastane Otomasyonu</title>
+</head>
+<body>
+    <div class="ust_bar">
+        <a href="anasayfa.php"><h1>Hastane Otomasyonu</h1></a>
+        <div class="menu">
+            <a href="hesap.php"><h5>Hesap Bilgileri</h5></a>
+            <a href="randevu.php"><h5>Randevu Bilgileri</h5></a>
+
+        </div>
+    </div>
+    <div class="cikis">
+        Çıkış yap
+</div>
+    
+</body>
+</html>
